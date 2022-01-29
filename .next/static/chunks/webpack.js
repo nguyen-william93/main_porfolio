adb8e78fcdad01b705b4449be56de736c2836c73
nguyen-william93 <=>
2022-01-29 17:10:48 -0600

    Add a hero section including a button and background animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19143bacaaae0bdbfcb6"; }
+/******/ 		__webpack_require__.h = function() { return "cef1c492dcf07ab06f85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { ButtonFront, Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of ABCXYZ
+      </SectionText>
+      <Button onClick={() => window.location = "https://google.com"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
